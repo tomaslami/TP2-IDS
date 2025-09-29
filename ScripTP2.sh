e585097ad3d04f9074b6805e478424e52e0167e0
CortezD <66151763+CortezD@users.noreply.github.com>
2025-09-29 21:27:24 +0000

    Script inicial con ambiente virtual

---
+++ ScripTP2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+trabajopractico2="TP2-IDS"
+
+if python3 --version ; then
+    echo "Python instalado"
+else
+    echo "Debe instalar python"
+    sudo apt install -y python3
+fi
+
+if pip3 --version ; then
+    echo "Pip instalado"
+else
+    echo "Debe instalar Pip"
+    sudo apt install -y python3-pip
+fi
+
+sudo apt install python3-venv
+
+mkdir -p "$trabajopractico2"
+mkdir -p "$trabajopractico2"/.venv
+mkdir -p "$trabajopractico2"/static/css
+mkdir -p "$trabajopractico2"/static/images
+mkdir -p "$trabajopractico2"/templates
+
+cd "$trabajopractico2"
+
+touch app.py
+
+python3 -m venv .venv
+
+. .venv/bin/activate
+
+pip3 install Flask
